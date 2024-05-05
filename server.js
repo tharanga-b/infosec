f876338781965c2bd419c489df6d64687bd0a8ec
tharanga-b <167369092+tharanga-b@users.noreply.github.com>
2024-05-05 12:19:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ var app = express();
 app.disable("x-powered-by");
 var fs = require("fs");
 var path = require("path");
-var bycrypt = require('bcrypt');
 
 app.use(function (req, res, next) {
   res.set({
